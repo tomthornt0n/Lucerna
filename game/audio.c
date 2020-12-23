489dfa161a16fbc0a029b4f4ec211f23a1b60a2f
Tom Thornton <thomas.thornton40@gmail.com>
2020-12-23 13:14:34 +0000

    remove MIT license

--- game/audio.c
+++ game/audio.c
@@ -2,9 +2,8 @@
   Lucerna
 
   Author  : Tom Thornton
-  Updated : 28 Nov 2020
-  License : MIT, at end of file
-  Notes   : Just to get something playing.
+  Updated : 23 Dec 2020
+  License : N/A
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 #define SOURCE_HAS_FLAG(source, flag) (source->flags & flag)
@@ -186,28 +185,4 @@ set_audio_source_pan(AudioSource *source,
     source->pan = CLAMP(pan, 0.0f, 1.0f);
     recalculate_audio_source_gain(source);
 }
-
-/*
-MIT License
-
-Copyright (c) 2020 Tom Thornton
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 
--- game/renderer.c
+++ game/renderer.c
@@ -2,8 +2,8 @@
   Lucerna
 
   Author  : Tom Thornton
-  Updated : 21 Dec 2020
-  License : MIT, at end of file
+  Updated : 23 Dec 2020
+  License : N/A
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 #define BATCH_SIZE 256
@@ -1281,28 +1281,4 @@ process_render_queue(OpenGLFunctions *gl)
 
     arena_free_all(&global_frame_memory);
 }
-
-/*
-MIT License
-
-Copyright (c) 2020 Tom Thornton
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 
